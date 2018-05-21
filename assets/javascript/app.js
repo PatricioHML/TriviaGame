34d8ac57f5d46c1c4cbba8e736d8824034192af0
PatricioHML <patriciohml@hotmail.com>
2018-05-21 14:37:00 -0500

    antes del timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
     
     
-
+    function clear() {
+        $(".btn").empty();
+    }
     
-
     function submit() {
-        alert("submited");
+        
         var question1 = document.quiz.question1.value;
         var question2 = document.quiz.question2.value;
         var question3 = document.quiz.question3.value;
         var question4 = document.quiz.question4.value;
         var correct = 0;
        
+// ifs de las respuestas
 
         if(question1 == "nintendo") {
             correct++;
@@ -29,8 +31,34 @@ $(document).ready(function() {
         if (question4 == "goodGame") {
             correct++;
         }
+// aquí empiezan los ifs de los resultados
 
-        console.log(correct);
+        if (correct == 0) {
+            
+            $(".results").append("0!") ;
+            
+            
+        }
+
+        if (correct == 1) {
+            $(".results").append("25%!");
+            
+        }
+
+        if (correct == 2) {
+            $(".results").append("50%!");
+           
+        }
+
+        if (correct == 3) {
+            $(".results").append("75%!");
+            
+        }
+
+        if (correct == 4) {
+            $(".results").append("100%!");
+            
+        }
 
 
 
