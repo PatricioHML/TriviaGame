e92739003e88df482abf6bd2e0247d18526eea7d
PatricioHML <patriciohml@hotmail.com>
2018-05-21 13:51:16 -0500

    ya está jalando el contador

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,51 @@
 $(document).ready(function() {
+    
+    
+
+    
 
     function submit() {
         alert("submited");
+        var question1 = document.quiz.question1.value;
+        var question2 = document.quiz.question2.value;
+        var question3 = document.quiz.question3.value;
+        var question4 = document.quiz.question4.value;
+        var correct = 0;
+       
+
+        if(question1 == "nintendo") {
+            correct++;
+
+        }
+
+        if (question2 == "ff12") {
+            correct++;
+        }
+
+        if (question3 == "badDudes") {
+            correct++;
+        }
+
+        if (question4 == "goodGame") {
+            correct++;
+        }
+
+        console.log(correct);
+
+
+
     }
 
     $( ".btn" ).click(function() {
         $(".results").css({
             "visibility" : "visible"
-        })
+            
+        });
+        submit();
+        
+
+
+
     });
 
 
